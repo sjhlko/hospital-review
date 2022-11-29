7d3fda2eaac361071198ba4e47710386345c1289
서정희 <sjhlko@naver.com>
2022-11-29 15:19:08 +0900

    로그인시 예외처리(id 없음, 비밀번호 매칭 안됨)추가

---
+++ src/main/java/com/hospital/hospitalreview/domain/dto/UserLoginResponse.java
@@ -0,0 +1,12 @@
+package com.hospital.hospitalreview.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class UserLoginResponse {
+    private String token;
+}
--- src/main/java/com/hospital/hospitalreview/exception/ErrorCode.java
+++ src/main/java/com/hospital/hospitalreview/exception/ErrorCode.java
@@ -7,7 +7,11 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 @Getter
 public enum ErrorCode {
-    DUPLICATED_USER_NAME(HttpStatus.CONFLICT,"User name duplicated");
+    DUPLICATED_USER_NAME(HttpStatus.CONFLICT,"User name duplicated"),
+    NOT_FOUND(HttpStatus.NOT_FOUND, "user doesn't exist"),
+
+    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "password is not matched")
+    ;
     private HttpStatus status;
     private String message;
 }
