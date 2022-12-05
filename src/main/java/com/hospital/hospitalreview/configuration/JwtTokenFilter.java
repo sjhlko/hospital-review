3575468d8281c55e14cee11d8b62a5ffd080655f
서정희 <sjhlko@naver.com>
2022-12-05 18:20:23 +0900

    user review 작성시 토큰의 만료 여부를 확인하는 로직 추가

--- src/main/java/com/hospital/hospitalreview/configuration/JwtTokenFilter.java
+++ src/main/java/com/hospital/hospitalreview/configuration/JwtTokenFilter.java
@@ -1,7 +1,14 @@
 package com.hospital.hospitalreview.configuration;
 
 import com.hospital.hospitalreview.service.UserService;
+import com.hospital.hospitalreview.utils.JwtTokenUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -9,14 +16,43 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @RequiredArgsConstructor
+@Slf4j
 public class JwtTokenFilter extends OncePerRequestFilter {
     private final String secretKey;
     private final UserService userService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        //
+
+        final String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        log.info("authorizationHeader : {}", authorizationHeader);
+
+        if(authorizationHeader==null || !authorizationHeader.startsWith("Bearer")){
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        String token;
+        try {
+           token = authorizationHeader.split(" ")[1];
+        }catch (Exception e){
+            log.error("token 추출에 실패함");
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        if(JwtTokenUtil.isExpired(token,secretKey)){
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken("", null, List.of(new SimpleGrantedAuthority("USER"))    );
+        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        filterChain.doFilter(request, response);
+
     }
 }
--- src/main/java/com/hospital/hospitalreview/utils/JwtTokenUtil.java
+++ src/main/java/com/hospital/hospitalreview/utils/JwtTokenUtil.java
@@ -7,6 +7,15 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import java.util.Date;
 
 public class JwtTokenUtil {
+
+    public static Claims extractClaims(String  token, String key){
+        return Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody();
+    }
+
+    public static boolean isExpired(String token, String secretKey){
+        Date expiredDate = extractClaims(token, secretKey).getExpiration();
+        return expiredDate.before(new Date());
+    }
     public static String createToken(String userName, String key, long expireTimeMs) {
         Claims claims = Jwts.claims(); // 일종의 map
         claims.put("userName", userName);
