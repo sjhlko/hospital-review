f81b300b092094c27141036f7192918e2376c490
서정희 <sjhlko@naver.com>
2022-12-05 19:41:36 +0900

    유저이름을 컨트롤러에서 받아오는것으로 수정

--- src/main/java/com/hospital/hospitalreview/controller/UserReviewController.java
+++ src/main/java/com/hospital/hospitalreview/controller/UserReviewController.java
@@ -1,17 +1,22 @@
 package com.hospital.hospitalreview.controller;
 
 import com.hospital.hospitalreview.domain.dto.ReviewCreateRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/user-reviews")
 public class UserReviewController {
 
     @PostMapping
-    public String write(@RequestBody ReviewCreateRequest dto){
-        return "리뷰 등록 성공";
+    public String write(@RequestBody ReviewCreateRequest dto, Authentication authentication){
+        log.info("Controller userName : {}", authentication.getName());
+        return "리뷰 등록 성공\n"+"유저 이름 :"+authentication.getName();
     }
 }
